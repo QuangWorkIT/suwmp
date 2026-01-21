99a27ac8c561bd9e697aa2626bd18e74695662b1
urjellyfish <annaphan0511@gmail.com>
2026-01-21 11:36:21 +0700

    [FEAT] Add payload to jwt

---
+++ suwmp_be/src/main/java/com/example/suwmp_be/entity/Token.java
@@ -0,0 +1,32 @@
+package com.example.suwmp_be.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "\"Token\"")
+public class Token {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "id", nullable = false)
+    private User user;
+
+    @Column(nullable = false)
+    private String tokenId;
+
+    @Column(nullable = false)
+    private LocalDateTime expiredAt;
+
+    @Column(insertable = false, updatable = false)
+    private LocalDateTime createdAt;
+}
--- suwmp_be/src/main/java/security/JwtUtil.java
+++ suwmp_be/src/main/java/security/JwtUtil.java
@@ -1,5 +1,6 @@
 package security;
 
+import com.example.suwmp_be.entity.User;
 import io.jsonwebtoken.JwtBuilder;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
@@ -11,6 +12,8 @@ import org.springframework.stereotype.Component;
 import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 @Slf4j
@@ -27,9 +30,14 @@ public class JwtUtil {
         this.secretKey = Keys.hmacShaKeyFor(jwtSecret.getBytes(StandardCharsets.UTF_8));
     }
 
-    public String generateToken(String email) {
+    public String generateToken(User user) {
+        Map<String, String> payload = new HashMap<>();
+        payload.put("fullName", user.getFullName());
+        payload.put("role", user.getRole().getName());
+        payload.put("email", user.getEmail());
         return Jwts.builder()
-                .subject(email)
+                .subject(user.getId().toString())
+                .claims(payload)
                 .issuedAt(new Date())
                 .expiration(new Date((new Date()).getTime() + jwtExpiration))
                 .signWith(secretKey)
