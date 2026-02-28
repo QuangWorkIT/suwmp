599382a7df1dcff28ca997e9960f008a2c3f0574
Hải Phúc <haiphuchoang2675@gmail.com>
2026-02-28 15:20:51 +0700

    [REFACTOR] Updated route for admin complaint page

--- suwmp_fe/src/routes/admin.route.tsx
+++ suwmp_fe/src/routes/admin.route.tsx
@@ -2,6 +2,7 @@ import { AdminMain } from "@/components/layout/admin";
 import { AdminDashboardPage, UserManagementPage, WasteCategoriesPage } from "@/pages/admin";
 import { Navigate } from "react-router";
 import ProtectedRoute from "./ProtectedRoute";
+import ComplaintsPage from "@/pages/admin/ComplaintPage";
 
 export const adminRoutes = [
     {
@@ -17,12 +18,12 @@ export const adminRoutes = [
                     { path: "waste-categories", element: <WasteCategoriesPage /> },
                     { path: "regions", element: <div>Regions & Zones</div> },
                     { path: "policies", element: <div>Policies</div> },
-                    { path: "complaints", element: <div>Complaints</div> },
+                    { path: "complaints", element: <ComplaintsPage /> },
                     { path: "audit-logs", element: <div>Audit Logs</div> },
                     { path: "analytics", element: <div>Analytics</div> },
                     { path: "access-control", element: <div>Access Control</div> },
                 ],
-            }
-        ]
+            },
+        ],
     },
-]
+];
